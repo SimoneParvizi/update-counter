b6ab37990aefd550c42c8b956c5f0590a54f2152
SimoneParvizi <simone.parvizi@outlook.it>
2025-01-24 16:06:34 +0100

    trigger

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+echo "Stopping the container"
 docker stop contribution-bot
+echo "Removing the container"
 docker rm contribution-bot
 docker build --no-cache --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_ed25519_simoneparvizi)" \
              --build-arg SSH_CONFIG="$(cat ~/.ssh/config)" \
-             -t your-image-name .
+             -t contribution-bot .
 
 docker run -d --name contribution-bot contribution-bot
